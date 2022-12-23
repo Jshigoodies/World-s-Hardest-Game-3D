25b4dba48967fee917f6491baad57f9bf4182515
Jason Shi <jshigoodies@gmail.com>
2022-12-22 18:44:35 -0600

    finished game

--- Start_Screen.gd
+++ Start_Screen.gd
@@ -14,3 +14,7 @@ func _on_Button_pressed():
 func _physics_process(delta):
 	if Input.is_action_pressed("f") and Input.is_action_pressed("k"):
 		get_tree().change_scene("res://assets/Test Level.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://assets/SpeedRun.tscn")
